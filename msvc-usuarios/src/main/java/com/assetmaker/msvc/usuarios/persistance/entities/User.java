c58e322c5721c12c084e9d54ae443adc4be2a3af
sebastian <sebastian.cifuentes0808@gmail.com>
2024-09-15 13:43:32 -0500

    Servicio update y delete completados

--- msvc-usuarios/src/main/java/com/assetmaker/msvc/usuarios/persistance/entities/User.java
+++ msvc-usuarios/src/main/java/com/assetmaker/msvc/usuarios/persistance/entities/User.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import org.hibernate.validator.constraints.UniqueElements;
 
@@ -21,15 +22,21 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     @NotBlank
+    @NotNull
     private String first_name;
     @NotBlank
+    @NotNull
     private String last_names;
     @NotBlank
     @Email
+    @NotNull
+    @Column(unique = true)
     private String email;
     @NotBlank
+    @NotNull
     private String password;
     @Enumerated(EnumType.STRING)
     @Column(name = "risk_profile", nullable = false)
+    @NotNull
     private RiskProfile risk_profile;
 }
